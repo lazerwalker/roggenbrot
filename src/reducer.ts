211a9196ff863b34a8ca8722691076670a4b3eec
Mike Lazer-Walker <michael@lazerwalker.com>
2018-03-05 18:44:55 +0100

    Properly dispose of captured piece

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,16 +1,19 @@
 import State from "./state";
 import { Action, ActionType } from "./action";
 import * as _ from "lodash";
-import { xyToPos } from './Piece';
+import Piece, { xyToPos } from './Piece';
 
 export default function(state: State, action: Action): State {
   switch (action.type) {
     case ActionType.Move:
       const {piece, to} = action.value
 
-      const newPieces = _.filter(state.pieces, (p) => p.pos !== piece.pos)
+      let newPieces: Piece[] = _.filter(state.pieces, (p) => p.pos !== piece.pos)
 
       const capturedPiece = _.find(state.pieces, (p) => p.x === to.x && p.y === to.y)
+      if (capturedPiece) {
+        newPieces = _.without(newPieces, capturedPiece)
+      }
 
       const newPiece = {...piece,
         x: to.x,
