b362e15c0c992cc5ff117d396bb31ceddaa79e08
Mike Lazer-Walker <michael@lazerwalker.com>
2018-03-08 00:42:10 +0100

    Properly animate knight movement

--- src/animationTick.ts
+++ src/animationTick.ts
@@ -5,32 +5,31 @@ export default function animationTick(state: State): State {
   const newState = {...state}
   newState.pieces = newState.pieces.map((p) => {
     if (p.destination) {
-      if (p.piece === PieceType.Knight) {
-        // TODO:
-        const newKnight = {
-          ...p,
-          x: p.destination.x,
-          y: p.destination.y,
-          pos: xyToPos(p.destination.x, p.destination.y)
-        }
-
-        delete newKnight.destination
-        return newKnight
-      }
-
       let newX = p.x
       let newY = p.y
 
-      if (p.destination.x > p.x) {
-        newX += 1
-      } else if (p.destination.x < p.x) {
-        newX -= 1
-      }
+      if (p.piece === PieceType.Knight) {
+        if (p.destination.x > p.x) {
+          newX += 1
+        } else if (p.destination.x < p.x) {
+          newX -= 1
+        } else if (p.destination.y > p.y) {
+          newY += 1
+        } else if (p.destination.y < p.y) {
+          newY -= 1
+        }
+      } else {
+        if (p.destination.x > p.x) {
+          newX += 1
+        } else if (p.destination.x < p.x) {
+          newX -= 1
+        }
 
-      if (p.destination.y > p.y) {
-        newY += 1
-      } else if (p.destination.y < p.y) {
-        newY -= 1
+        if (p.destination.y > p.y) {
+          newY += 1
+        } else if (p.destination.y < p.y) {
+          newY -= 1
+        }
       }
 
       const newPiece = {
