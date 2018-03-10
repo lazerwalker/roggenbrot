42df46c4bddbb1f286be5de89e121a73cc9123b5
Mike Lazer-Walker <michael@lazerwalker.com>
2018-03-10 15:30:22 +0100

    Allow free movement in menu

--- src/menu/reducer.ts
+++ src/menu/reducer.ts
@@ -1,30 +1,35 @@
-import State, { GameMode, getPlayer } from "../state";
+import State, { GameMode } from "../state";
 import { Action, ActionType, MoveAction } from "../action";
-import { PieceType, xyToPos } from "../Piece";
+import Piece, { PieceType, xyToPos } from "../Piece";
 import changeMode from "../changeMode";
+import * as _ from "lodash";
 
 export default function(state: State, action: Action): State {
   if (action.type === ActionType.Move) {
-    const to = (action as MoveAction).value.to
+    const {piece, to} = (action as MoveAction).value
 
-    const piece = state.board.pieces.find((p) => p.x === to.x && p.y === to.y)
+    let pieces: Piece[] = _.filter(state.board.pieces, (p) => p.pos !== piece.pos)
 
-    if (piece && piece.piece === PieceType.King) {
-      const player = getPlayer(state.board)
-      if (player) {
-        const newPlayer = {
-          ...player,
-          x: to.x,
-          y: to.y,
-          pos: xyToPos(to.x, to.y),
-          piece: PieceType.King
-        }
-        const pieces = [newPlayer]
-        const newState = {...state, board: {...state.board, pieces}}
-        return changeMode(newState, GameMode.Game)
-      }
-      return state
+    const capturedPiece = _.find(state.board.pieces, (p) => p.x === to.x && p.y === to.y)
+    if (capturedPiece) {
+      pieces = _.without(pieces, capturedPiece)
     }
+
+    const newPiece = {...piece,
+      x: to.x,
+      y: to.y,
+      pos: xyToPos(to.x, to.y),
+      piece: (capturedPiece ? capturedPiece.piece : piece.piece)
+    }
+
+    pieces.push(newPiece)
+    const newState = {...state, board: {...state.board, pieces}}
+
+    if (capturedPiece && capturedPiece.piece === PieceType.King) {
+        return changeMode(newState, GameMode.Game)
+    }
+
+    return newState
   }
   return state
 }
